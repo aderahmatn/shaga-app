affa5407a2ffcb29d66e90c90f55d5ac226c8cd8
aderahmatn <nurdiyana.ade@gmail.com>
2023-08-29 19:54:29 +0700

    test telgram webhook

--- application/controllers/Telegram.php
+++ application/controllers/Telegram.php
@@ -26,25 +26,26 @@ class Telegram extends CI_Controller
             $update = json_decode($content, TRUE);
             $chatID = $update["message"]["chat"]["id"];
             $message = $update["message"]["text"];
-
-            if (strpos($message, "/start") === 0) {
-                $txtok = urlencode("SELAMAT DATANG DI GISAKA AUTOMATION SYSTEM");
-                file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txtok.&parse_mode=HTML");
-                // $user = $this->User_m->get_by_chat_id($chatID);
-                // if (!$user) {
-                //     $txtok = urlencode("SELAMAT DATANG DI GISAKA AUTOMATION SYSTEM");
-                //     file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txtok.&parse_mode=HTML");
-                // } else {
-                //     $txt = urlencode("<b>SELAMAT DATANG</b>\nNama : " . strtoupper($user->nama_lengkap) . "\nNIP : " . strtoupper($user->nip));
-                //     file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txt&parse_mode=HTML");
-                // }
-            } else {
-                $txtok = urlencode("SELAMAT DATANG DI GISAKA AUTOMATION SYSTEM \n saat ini GISAKA AUTOMATION SYSTEM dalam pengembangan.");
-                file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txtok.&parse_mode=HTML");
-            }
-        } else {
-            echo 'Not authorized!';
-        }
+            $txtok = urlencode("SELAMAT DATANG DI GISAKA AUTOMATION SYSTEM");
+            file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txtok.&parse_mode=HTML");
+        //     if (strpos($message, "/start") === 0) {
+        //         $txtok = urlencode("SELAMAT DATANG DI GISAKA AUTOMATION SYSTEM");
+        //         file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txtok.&parse_mode=HTML");
+        //         // $user = $this->User_m->get_by_chat_id($chatID);
+        //         // if (!$user) {
+        //         //     $txtok = urlencode("SELAMAT DATANG DI GISAKA AUTOMATION SYSTEM");
+        //         //     file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txtok.&parse_mode=HTML");
+        //         // } else {
+        //         //     $txt = urlencode("<b>SELAMAT DATANG</b>\nNama : " . strtoupper($user->nama_lengkap) . "\nNIP : " . strtoupper($user->nip));
+        //         //     file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txt&parse_mode=HTML");
+        //         // }
+        //     } else {
+        //         $txtok = urlencode("SELAMAT DATANG DI GISAKA AUTOMATION SYSTEM \n saat ini GISAKA AUTOMATION SYSTEM dalam pengembangan.");
+        //         file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txtok.&parse_mode=HTML");
+        //     }
+        // } else {
+        //     echo 'Not authorized!';
+        // }
     }
     public function send()
     {
