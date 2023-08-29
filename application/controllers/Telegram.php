c387833b29fc313e7045e5f84a349b97f97cd1df
aderahmatn <nurdiyana.ade@gmail.com>
2023-08-29 20:08:48 +0700

    test telgram webhook

--- application/controllers/Telegram.php
+++ application/controllers/Telegram.php
@@ -20,31 +20,16 @@ class Telegram extends CI_Controller
     public function index()
     {
         $content = file_get_contents("php://input");
-        if ($content) {
-            $TOKEN = "6259502863:AAEsTD1linSz1FbX4Hs7SH5U238u_ftIRZU";
-            $apiURL = "https://api.telegram.org/bot$TOKEN";
-            $update = json_decode($content, TRUE);
-            $chatID = $update["message"]["chat"]["id"];
-            $message = $update["message"]["text"];
+        $TOKEN = "6259502863:AAEsTD1linSz1FbX4Hs7SH5U238u_ftIRZU";
+        $apiURL = "https://api.telegram.org/bot$TOKEN";
+        $update = json_decode($content, TRUE);
+        $chatID = $update["message"]["chat"]["id"];
+        $message = $update["message"]["text"];
+        $txtok = urlencode("SELAMAT DATANG DI GISAKA AUTOMATION SYSTEM");
+        file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txtok.&parse_mode=HTML");
+        if ($message) {
             $txtok = urlencode("SELAMAT DATANG DI GISAKA AUTOMATION SYSTEM");
             file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txtok.&parse_mode=HTML");
-            // if (strpos($message, "/start") === 0) {
-            //     $txtok = urlencode("SELAMAT DATANG DI GISAKA AUTOMATION SYSTEM");
-            //     file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txtok.&parse_mode=HTML");
-            //     // $user = $this->User_m->get_by_chat_id($chatID);
-            //     // if (!$user) {
-            //     //     $txtok = urlencode("SELAMAT DATANG DI GISAKA AUTOMATION SYSTEM");
-            //     //     file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txtok.&parse_mode=HTML");
-            //     // } else {
-            //     //     $txt = urlencode("<b>SELAMAT DATANG</b>\nNama : " . strtoupper($user->nama_lengkap) . "\nNIP : " . strtoupper($user->nip));
-            //     //     file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txt&parse_mode=HTML");
-            //     // }
-            // } else {
-            //     $txtok = urlencode("SELAMAT DATANG DI GISAKA AUTOMATION SYSTEM \n saat ini GISAKA AUTOMATION SYSTEM dalam pengembangan.");
-            //     file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=$txtok.&parse_mode=HTML");
-            // }
-        } else {
-            echo 'Not authorized!';
         }
 
     }
